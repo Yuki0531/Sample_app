22957064402880d77d9736e671888855a61e7b50
EC2 Default User <ec2-user@ip-172-31-27-194.ap-southeast-1.compute.internal>
2019-09-16 13:32:53 +0000

    failed chapter 06

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
-
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
 end
