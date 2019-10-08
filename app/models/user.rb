c8db63765db83d932a410420e48ba2198f2bc7d6
EC2 Default User <ec2-user@ip-172-31-27-194.ap-southeast-1.compute.internal>
2019-10-08 14:04:50 +0000

    Add account activation2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -41,9 +43,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # アカウントを有効にする
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   # 有効化用のメールを送信する
